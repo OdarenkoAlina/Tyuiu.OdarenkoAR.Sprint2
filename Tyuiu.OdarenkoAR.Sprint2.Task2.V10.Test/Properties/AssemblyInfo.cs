2dda51dda7613a6fb4fe6b47dd37e7c0a2de06ed
Tyuiu.OdarenkoAR@outlook.com <odarenkoalina@mail.ru>
2023-10-23 18:48:22 +0500

    Task2

---
+++ Tyuiu.OdarenkoAR.Sprint2.Task2.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.OdarenkoAR.Sprint2.Task2.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.OdarenkoAR.Sprint2.Task2.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5431c7dc-e81d-463d-9ea3-7f30614bee68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.OdarenkoAR.Sprint2.Task2.V10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.OdarenkoAR.Sprint2.Task2.V10.Lib;
+namespace Tyuiu.OdarenkoAR.Sprint2.Task2.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнила: Одаренко А. Р. | ИСПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнила Одаренко А. Р. | ИСПб-23-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры  *");
+            Console.WriteLine("* и вычисляет находится ли точка с координатами X, Y в заштрихованной     *");
+            Console.WriteLine("* области.                                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите значение переменной X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+            Console.ReadKey();
+        }
+    }
+}
