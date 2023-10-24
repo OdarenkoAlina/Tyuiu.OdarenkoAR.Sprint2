d053adc551c069eadcc963c7060b35ebe2a5e39e
Tyuiu.OdarenkoAR@outlook.com <odarenkoalina@mail.ru>
2023-10-24 21:25:32 +0500

    Task5

---
+++ Tyuiu.OdarenkoAR.Sprint2.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.OdarenkoAR.Sprint2.Task5.V3.Lib
+{
+    public class DataService : ISprint2Task5V3
+    {
+        public string FindDayName(int k)
+        {
+            string res;
+            switch(k)
+            {
+                case 1:
+                    res = "Понедельник";
+                    break;
+                case 2:
+                    res = "Вторник";
+                    break;
+                case 3:
+                    res = "Среда";
+                    break;
+                case 4:
+                    res = "Четверг";
+                    break;
+                case 5:
+                    res = "Пятница";
+                    break;
+                case 6:
+                    res = "Суббота";
+                    break;
+                case 7:
+                    res = "Воскресенье";
+                    break;
+                default:
+                    res = "Некорректный день недели";
+                    break;
+            }
+            return res;
+        }
+    }
+}
